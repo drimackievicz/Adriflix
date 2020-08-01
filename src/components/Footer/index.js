f16e7311ab92c0fc8dd9aeb433b37eb89af83e0c
Adriana <drimackievicz@gmail.com>
2020-08-01 12:12:51 -0300

    CadastroCategorias

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import Logo from '../../assets/img/adriflix.png';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="https://www.linkedin.com/in/adriana-mackievicz-44565a34npx">
+        <img src={Logo} alt="AdriFlixx" />
       </a>
       <p>
         Orgulhosamente criado durante a
